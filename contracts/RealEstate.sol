dba3be088d5964e4d636f73197bde5d23a6b2e0b
James Takaendisa <james.takaendisa01@gmail.com>
2024-02-24 17:02:38 +0200

    feature: add RealEstate smart contract

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -5,13 +5,13 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract RealEstate is ERC721URIStorage {
+contract RealEstate is ERC721URIStorage{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() ERC721("Real Estate", "REAL") {}
 
-    function mint(string memory tokenURI) public returns (uint256) {
+    function mint(string memory tokenURI) public returns(uint256){
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
@@ -21,7 +21,8 @@ contract RealEstate is ERC721URIStorage {
         return newItemId;
     }
 
-    function totalSupply() public view returns (uint256) {
+    function totalSupply() public view returns(uint256) {
         return _tokenIds.current();
     }
 }
+ 

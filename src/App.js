661f7281837ffc27c47915bf9c68c31f1fa63e6b
James Takaendisa <james.takaendisa01@gmail.com>
2024-02-26 12:51:12 +0200

    link RealEstate smart contract to frontend

--- src/App.js
+++ src/App.js
@@ -14,23 +14,64 @@ import Escrow from './abis/Escrow.json';
 import config from './config.json';
 
 function App() {
+  const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
 
   useEffect(() => {
     const loadBlockchainData = async () => {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setProvider(provider);
 
-      setAccount(accounts[0]);
+      const network = await provider.getNetwork();
+
+      const realEstate = new ethers.Contract(
+        config[network.chainId].realEstate.address,
+        RealEstate,
+        provider
+      );
+
+      try {
+        const totalSupply = await realEstate.totalSupply();
+        console.log('Total Supply:', totalSupply);
+      } catch (error) {
+        console.error('Error fetching total supply:', error);
+      }
+      // const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
+
+      window.ethereum.on('accountsChanged', async () => {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        const account = ethers.utils.getAddress(accounts[0]);
+        setAccount(account);
+      });
     };
-
     loadBlockchainData();
-  }, [account]);
+  }, []);
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
       <div className="cards__section">
-        <h3>Welcome to Millow!</h3>
+        <h3>Homes for you</h3>
+
+        <hr />
+
+        <div className="cards">
+          <div className="card">
+            <div className="card__image">
+              <img src="" alt="Home" />
+            </div>
+            <div className="card__info">
+              <h4>1 ETH</h4>
+              <p>
+                <strong>1</strong> bds |<strong>2</strong> ba |<strong>3</strong> sqft
+              </p>
+              <p>1234 Elm St</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
